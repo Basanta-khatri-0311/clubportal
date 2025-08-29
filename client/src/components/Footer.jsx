941a46f93764e12cf126d80e9109539e4d4bb2ef
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-08-29 20:33:33 +0545

    restarting everything

--- client/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-// src/components/Footer.jsx
-export default function Footer() {
-  return (
-    <footer className="bg-green-800 text-white text-center py-6 font-poppins">
-      <p>&copy; 2025 Sangar Samuha Football Club. All rights reserved.</p>
-    </footer>
-  );
-}
